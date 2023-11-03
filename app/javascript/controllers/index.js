95b0ddb5f308a275db0b999fb39b0efc63722532
Miguel Diaz <mdiaz.git+belial@codescrum.com>
2023-11-03 14:55:09 -0500

    register cocooned controller into app/javascript/controllers/index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import CocoonedController from "./cocooned_controller"
+application.register("cocooned", CocoonedController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
