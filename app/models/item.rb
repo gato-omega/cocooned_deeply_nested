b275aab83e4bff6db73196238ba75b32f1b8aaca
Miguel Diaz <mdiaz.git+belial@codescrum.com>
2023-11-03 11:07:21 -0500

    update models with accepts_nested_attributes_for and relevant associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :list
+
+  has_many :subitems, inverse_of: :item
+  accepts_nested_attributes_for :subitems, reject_if: :all_blank, allow_destroy: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ApplicationRecord
+  has_many :items, inverse_of: :list
+  accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
 end
